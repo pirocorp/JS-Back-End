1017d7af693ae7bd63a274de0bfd57d2c8def84b
Zdravko Zdravkov <zdr0@yahoo.com>
2022-10-04 11:21:55 +0200

    Simplifying mongoose service

--- 04. NoSQL and MongoDB/Mongoose Demo/common/services/MongooseService.ts
+++ 04. NoSQL and MongoDB/Mongoose Demo/common/services/MongooseService.ts
@@ -4,13 +4,10 @@ const connectionString = "mongodb://localhost:27017/testdb2";
 const log = console.log;
 
 class MongooseService {
-    private retrySeconds = 5;
-    
+    private retrySeconds = 5;    
     private count = 0;
-    private connectionString: string = '';
 
-    constructor(connectionString: string) {
-        this.connectionString = connectionString;
+    constructor() {
 
         this.connectWithRetry();
     };
@@ -23,15 +20,15 @@ class MongooseService {
         log('Attempting MongoDB connection (will retry if needed)');
 
         mongoose
-            .connect(this.connectionString)
+            .connect(connectionString)
             .then(() => log('MongoDB is connected'))
             .catch(err => {
                 log(`MongoDB connection unsuccessful (will retry #${++this.count} after ${this.retrySeconds} seconds): ${err}`);
                 setTimeout(this.connectWithRetry, this.retrySeconds * 1000);
                 this.retrySeconds *= 1.5;
             });
-    }
+    };
 };
 
 // Modules are only evaluated once.
-export default new MongooseService(connectionString);
+export default new MongooseService();
