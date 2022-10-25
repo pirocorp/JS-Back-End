e08a01fde5af71898751a14c1550da70f3738a68
Zdravko Zdravkov <34960418+pirocorp@users.noreply.github.com>
2022-10-25 16:23:49 +0200

    Delete Workshops/DDD REST API directory

--- Workshops/DDD REST API/users/dto/patch.user.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PutUserDto } from './put.user.dto';
-
-// Partial feature from TypeScript, creates a new type by copying another 
-// type and making all its fields optional
-export interface PatchUserDto extends Partial<PutUserDto> {}
